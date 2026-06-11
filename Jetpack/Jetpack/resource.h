ff8b1cf999ef38cf590eeff1f21ef175faab5e2f
김성현 <seonghyeon2651@gmail.com>
2026-06-12 04:16:03 +0900

    FuelTank 리소스만 업로드
    자야겠다

--- Jetpack/Jetpack/resource.h
+++ Jetpack/Jetpack/resource.h
@@ -85,12 +85,13 @@
 #define IDB_BITMAP80                    203
 #define IDB_BITMAP81                    204
 #define IDB_BITMAP82                    205
+#define IDB_BITMAP83                    206
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        206
+#define _APS_NEXT_RESOURCE_VALUE        207
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
